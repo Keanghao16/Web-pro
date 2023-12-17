8a6b9b8a2f05571fccd6d3a0cc3ff12b3c08dbde
cypher__16 <124254912+Keanghao16@users.noreply.github.com>
2023-12-17 14:39:34 +0700

    Add files via upload

---
+++ scripts/index1.js
@@ -0,0 +1,57 @@
+function toggle1() {
+  toggleForms('block', 'none');
+}
+
+function toggle2() {
+  toggleForms('none', 'block');
+}
+
+function toggleForms(loginDisplay, signupDisplay) {
+  var blur = document.getElementById('blur');
+  var popup = document.getElementById('popup');
+
+  blur.classList.toggle('active');
+  popup.classList.toggle('active');
+
+  loginForm.style.display = loginDisplay;
+  signupForm.style.display = signupDisplay;
+
+  document.body.classList.toggle('popup-active');
+}
+
+
+
+
+const pwShowHide = document.querySelectorAll(".eye-icon"),
+loginLink = document.getElementById("loginLink"),
+signupLink = document.getElementById("signupLink"),
+loginForm = document.getElementById("loginForm"),
+signupForm = document.getElementById("signupForm");
+
+
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click", () => {
+    let pwField = eyeIcon.parentElement.querySelector(".password");
+
+    if (pwField.type === "password") {
+      pwField.type = "text";
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+    } else {
+      pwField.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    }
+  });
+});
+
+loginLink.addEventListener("click", () => {
+  // Show login form and hide signup form
+  loginForm.style.display = "block";
+  signupForm.style.display = "none";
+});
+
+signupLink.addEventListener("click", () => {
+  // Show signup form and hide login form
+  loginForm.style.display = "none";
+  signupForm.style.display = "block";
+});
+
